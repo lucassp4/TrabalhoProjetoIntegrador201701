f7552581b22a552c5f1c1037369bd1a8328c2272
Erikrit <Erikri_1995@hotmail.com>
2017-04-16 20:34:31 -0300

    Add files via upload

---
+++ TrabalhoProjetoIntegrador201701-master/TrabalhoProjetoIntegrador201701-master/src/application/Main.java
@@ -0,0 +1,26 @@
+package Application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane root = FXMLLoader.load(getClass().getResource("/View/PaginaLogin.fxml"));
+			Scene scene = new Scene(root,450,280);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ TrabalhoProjetoIntegrador201701-master/TrabalhoProjetoIntegrador201701-master/src/controller/ControllerSegundaPagina.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerSegundaPagina implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void cadEquipamento(){
+		
+	}
+	
+}
