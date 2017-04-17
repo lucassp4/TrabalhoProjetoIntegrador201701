e093908d5ad84f83736d5e636b56beadd37ad236
lucassp4 <lucassp4@gmail.com>
2017-04-17 01:07:06 -0300

    Commit

--- src/DAO/LoginDAO.java
+++ src/DAO/LoginDAO.java
@@ -1,7 +1,28 @@
 package DAO;
 
+import daoUtil.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by lucas.pereira on 17/04/2017.
  */
 public class LoginDAO {
+
+    private Connection con;
+    private Statement stmt;
+    private PreparedStatement stm;
+
+
+    private String sqlLogin = "Select * from login";
+
+    public LoginDAO() throws SQLException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        con = factory.getConnection();
+
+    }
 }
